e9ccc409f9bcf55a7947aff3b59b16f299541b4f
wilsonmarutti <45435418+wilsonmarutti@users.noreply.github.com>
2020-06-26 21:38:06 -0300

    detecção da parede pela barra

--- script.js
+++ script.js
@@ -5,7 +5,23 @@ let x = canvas.width / 2;// inicial horizontal
 let y = canvas.height - 30; // inicial vertical
 let dx = 2; // variação horizontal
 let dy = -2;//variação vertical
-let ballRadius = 10;
+let ballRadius = 10; // raio da bola 
+
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let paddleY = (canvas.height - paddleHeight) - 20;
+let rightPressed = false;
+let leftPressed = false;
+
+
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX,paddleY,paddleWidth,paddleHeight); 
+    ctx.fillStyle = "#0095DD"; 
+    ctx.fill(); 
+    ctx.closePath();
+}
 
 function drawBall(){
     ctx.beginPath();
@@ -18,6 +34,7 @@ function drawBall(){
 function draw(){
     ctx.clearRect(0,0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
     //verifica se a bola sai na horizontal
     if(x + dx > canvas.width - ballRadius || x + dx< ballRadius){
         dx = -dx; // inverte sinal
@@ -25,7 +42,45 @@ function draw(){
     if(y + dy >canvas.height - ballRadius || y + dy <ballRadius){
         dy = -dy;
     }
+
+    if(rightPressed){
+        paddleX +=7;
+        if(paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }else if (leftPressed){
+        paddleX -=7;
+        if(paddleX<0){
+            paddleX = 0;
+        }
+
+    }
+
     x += dx;
     y += dy;
 }
 setInterval(draw,10);
+
+function keyDownHandler(e){
+    if(e.key == "Right" || e.key == "ArrowRight"){
+        rightPressed = true;
+    }
+    if(e.key == "left" || e.key == "ArrowLeft"){
+        leftPressed = true;
+    }
+
+}
+
+function keyUpHandler(e){
+    if(e.key == "Right" || e.key == "ArrowRight"){
+        rightPressed = false;
+    }
+    if(e.key == "left" || e.key == "ArrowLeft"){
+        leftPressed = false;
+    }
+
+}
+
+// adiciona evento de controle para o teclado
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
