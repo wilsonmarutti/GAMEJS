1f7cc942ef434ab8b10c07aab2a808bd53785d14
wilsonmarutti <45435418+wilsonmarutti@users.noreply.github.com>
2020-06-26 21:04:26 -0300

    aprimoramento de codigo JS,  bola detecta parede.

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ let x = canvas.width / 2;// inicial horizontal
 let y = canvas.height - 30; // inicial vertical
 let dx = 2; // variação horizontal
 let dy = -2;//variação vertical
+let ballRadius = 10;
 
 function drawBall(){
     ctx.beginPath();
-    ctx.arc(x,y,10,0,Math.PI*2);
+    ctx.arc(x,y,ballRadius,0,Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -17,6 +18,13 @@ function drawBall(){
 function draw(){
     ctx.clearRect(0,0, canvas.width, canvas.height);
     drawBall();
+    //verifica se a bola sai na horizontal
+    if(x + dx > canvas.width - ballRadius || x + dx< ballRadius){
+        dx = -dx; // inverte sinal
+    }
+    if(y + dy >canvas.height - ballRadius || y + dy <ballRadius){
+        dy = -dy;
+    }
     x += dx;
     y += dy;
 }
